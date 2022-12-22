f93125954243baebd59666078c876dc35611b59b
adam <hacaevadam@gmail.com>
2022-12-22 21:29:53 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'стул',
+          img: 'stul.jpg',
+          desc: 'lorem ipsum dolor',
+          category: 'chairs',
+          price: '49.99'
+
+        },
+        {
+          id: 2,
+          title: 'стол',
+          img: 'stol.jpg',
+          desc: 'lorem ipsum dolor',
+          category: 'tables',
+          price: '490.99'
+
+        },
+        {
+          id: 3,
+          title: 'шкаф',
+          img: 'shkaf.jpg',
+          desc: 'lorem ipsum dolor',
+          category: 'closets',
+          price: '799.99'
+
+        },
+        {
+          id: 4,
+          title: 'торшер',
+          img: 'torsher.jpg',
+          desc: 'lorem ipsum dolor',
+          category: 'lamps',
+          price: '99.99'
+
+        },
+        {
+          id: 5,
+          title: 'диван',
+          img: 'divan.webp',
+          desc: 'lorem ipsum dolor',
+          category: 'sofas',
+          price: '999.99'
+
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="wrapper"> 
+        <Header/>
+        <Items items={this.state.items}/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+
+export default function Header() {
+
+  let [cartOpen, setCartOpen] = useState(false)
+
+  return (
+    <header> 
+        <div>
+            <span className='logo'>House Staff</span>
+            <ul className='nav'>
+              <li>Про нас</li>
+              <li>Контакты</li>
+              <li>Кабинет</li>
+            </ul>
+            <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen )} className= {`shop-cart-button ${cartOpen && 'active'}`}/>
+
+            {cartOpen && (
+              <div className="shop-cart">
+                
+              </div>
+            )}
+
+
+        </div>
+        <div className='presentation'></div>
+    </header>
+  )
+}
